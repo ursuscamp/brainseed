fbf2b9c862dc8d7e6591bb733434c72304f3da03
Ryan Breen <ryan@breen.xyz>
2022-08-27 13:15:04 -0400

    Basic seed generation.
    Given an input, it can generate basic entropy for the generation of a
    seed phrase.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use sha2::{Digest, Sha256};
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::env::args().nth(1);
+
+    if input.is_none() {
+        eprintln!("Please specify input phrase.");
+        return;
+    }
+
+    let input = input.unwrap();
+    println!("Input value: {input:#?}");
+
+    let mut hasher = Sha256::new();
+    hasher.update(&input);
+    let result = hasher.finalize()[..16].to_vec();
+
+    let seed_phrase = bip39::Mnemonic::from_entropy(&result).unwrap();
+    println!("Final seed phrase: {seed_phrase}");
 }
